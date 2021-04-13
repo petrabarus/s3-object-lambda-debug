d3c6d63c0138869a011dbf7b447bed5a3d367a3e
EC2 Default User <ec2-user@ip-172-31-22-59.ap-southeast-1.compute.internal>
2021-04-13 06:42:51 +0000

    feat: display error message.

--- lambda/src/index.php
+++ lambda/src/index.php
@@ -12,19 +12,34 @@ return function ($event, $context) {
     $response = $client->get($url);
     $contents = (string) $response->getBody();
     
+    $region = $_ENV['AWS_REGION'];
     $s3Client = new S3Client([
         'version' => '2006-03-01',
-        'region' => $_ENV['AWS_REGION'],
+        'region' => $region,
+        //S3 Object Lambda workaround
+        //https://stackoverflow.com/a/66800385/688954
+        // 'endpoint' => "s3-object-lambda.{$region}.amazonaws.com",
+        // 'api' => [
+        //     'metadata' => [
+        //         'endpointPrefix' => 's3-object-lambda',
+        //     ]
+        // ]
+        //
     ]);
     $requestRoute = $getObjectContext['outputRoute'];
     $requestToken = $getObjectContext['outputToken'];
-    $s3Client->writeGetObjectResponse([
-        'Body' => strtoupper($contents),
-        'RequestRoute' => $requestRoute,
-        'RequestToken' => $requestToken,
-    ]);
-    
-    var_dump($contents);
+    $newBody = strtoupper($contents);
+    var_dump($newBody);
+    try {
+         $response = $s3Client->writeGetObjectResponse([
+            'Body' => $newBody,
+            'RequestRoute' => $requestRoute,
+            'RequestToken' => $requestToken,
+        ]);   
+    } catch (\Exception $ex) {
+        var_dump($ex->getMessage);
+    }
+    var_dump($response);
     
     return [
         'statusCode' => 200,
